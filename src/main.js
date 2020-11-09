c4cd25b2e80d82dd9f607ad6ce617d49213e3406
Giorgi Kikava <giogioqiqava777@gmail.com>
2020-11-09 15:08:32 +0400

    added Languages library

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
+import i18n from './i18n'
 import App from './App.vue'
 import router from './router'
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
